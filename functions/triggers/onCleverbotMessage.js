950f02a4f7d5428908e7824e07a778cb77c7014a
Zeeshan Ahmad <zeems1234@gmail.com>
2020-07-30 01:09:08 +0500

    added bot trigger

---
+++ functions/triggers/onCleverbotMessage.js
@@ -0,0 +1,32 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+const db = admin.firestore();
+
+const bot = {
+  displayName: 'cleverbot',
+  photoUrl: 'https://i.imgur.com/ydOMC2c.png',
+  uid: 'cleverbot',
+  status: {
+    lastChanged: new Date(),
+    state: 'online',
+  },
+  channels: {
+    general: true,
+  },
+};
+
+db.collection('users').doc(bot.uid).set(bot, { merge: true });
+
+module.exports = functions.firestore
+  .document('channels/general/messages/{messageId}')
+  .onCreate((doc, context) => {
+    const message = doc.data();
+    if (!message.text.startsWith('@cleverbot')) return;
+
+    return db.collection('channels/general/messages').add({
+      text: 'hey, whats up?',
+      user: db.collection('users').doc('cleverbot'),
+      createdAt: new Date(),
+    });
+  });
